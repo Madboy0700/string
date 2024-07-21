cdee68b3fa686eb750125ea2c6a2be881b39857e
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-07-21 20:05:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID", 28736869))
 API_HASH = getenv("API_HASH", "258e1a4c3dc48e0c46ee7c8c927ac4a3")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7489352285:AAEDwxDk-u4os2dzXsUdldjvEASfLw-fwC4")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://meyit31:Myt476331@cluster0.psvz6dk.mongodb.net/?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mongoguess:guessmongo@cluster0.zcwklzz.mongodb.net/?retryWrites=true&w=majority")
 
 OWNER_ID = int(getenv("OWNER_ID", 6510559004))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DelularSohbet")
