a54ae291afbab5ed2ce8f4ee7a9dc3c5adfd2b07
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-08-11 23:46:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 28736869))
-API_HASH = getenv("API_HASH", "258e1a4c3dc48e0c46ee7c8c927ac4a3")
+API_ID = int(getenv("API_ID", ))
+API_HASH = getenv("API_HASH", "")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7489352285:AAEDwxDk-u4os2dzXsUdldjvEASfLw-fwC4")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mongoguess:guessmongo@cluster0.zcwklzz.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 OWNER_ID = int(getenv("OWNER_ID", 6510559004))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DelularSohbet")
