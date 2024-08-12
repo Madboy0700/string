b8a2d97c94db32ccad0bcf5d450863e3efd51c09
Serkandgn81 <doganserkan344@gmail.com>
2024-08-13 01:26:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", ))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", 23915346))
+API_HASH = getenv("API_HASH", "3bc48fd6db8e9c1cfdc89c8de742d188")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7447707289:AAGl9BitaXMeBJBtX-v22gjcj3DfMQQCA2c")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://erkbwrs084:909090@cluster0.qdrfgmb.mongodb.net/?retryWrites=true&w=majority")
