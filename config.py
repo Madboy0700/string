d060a69865bae4c0f57ce0d2f6aa32178cdddec6
Serkandgn81 <doganserkan344@gmail.com>
2024-08-13 01:19:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = int(getenv("API_ID", ))
 API_HASH = getenv("API_HASH", "")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "7447707289:AAGl9BitaXMeBJBtX-v22gjcj3DfMQQCA2c")
-MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://erkbwrs084:909090@cluster0.qdrfgmb.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 6510559004))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DelularSohbet")
+OWNER_ID = int(getenv("OWNER_ID", 7305205222))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sohbetikidebir")
