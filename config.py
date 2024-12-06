59bab551aac75802c73272f66acf472bc24145ab
Madboy0700 <bobimobi07@gmail.com>
2024-12-06 09:10:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", 23915346))
+API_ID = int(getenv("API_ID", 7675940993))
 API_HASH = getenv("API_HASH", "3bc48fd6db8e9c1cfdc89c8de742d188")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7447707289:AAGl9BitaXMeBJBtX-v22gjcj3DfMQQCA2c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7972411299:AAGK2eJMA0pSWF3l0BqVWppqOrvFS8vjOVI")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://erkbwrs084:909090@cluster0.qdrfgmb.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 7305205222))
+OWNER_ID = int(getenv("OWNER_ID", 7675940993))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sohbetikidebir")
